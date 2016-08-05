f0b2e46304728a535fafe478e95412253d346553
belgrades <fcremaldc@gmail.com>
2016-08-05 17:08:48 -0400

    Adding __init__.py

---
+++ __init__.py
@@ -0,0 +1,12 @@
+"""Just some random thoughts.
+
+.. moduleauthor:: Fernando Crema <fcremaldc@gmail.com>
+
+"""
+
+import part_1
+import part_2
+
+
+def start():
+    "This should be something useful ..."
